f9b70cff07bc9089ddf3414127903eda4730c92c
Irvin Torres <irvinrx1996@hotmail.com>
2026-08-11 10:48:43 -0600

    fix(wasm): strictly validate Quantity type in format_call_arg

--- physure-wasm/src/phy_function.rs
+++ physure-wasm/src/phy_function.rs
@@ -91,17 +91,30 @@ fn format_call_arg(arg: &JsValue) -> Result<String, JsValue> {
     if let Some(s) = arg.as_string() {
         return Ok(s);
     }
-    if let Ok(to_string_val) = js_sys::Reflect::get(arg, &JsValue::from_str("toString")) {
-        if to_string_val.is_function() {
-            let func: js_sys::Function = to_string_val.unchecked_into();
-            if let Ok(res) = js_sys::Reflect::apply(&func, arg, &js_sys::Array::new()) {
-                if let Some(s) = res.as_string() {
-                    return Ok(s);
+    if is_quantity_instance(arg) {
+        if let Ok(to_string_val) = js_sys::Reflect::get(arg, &JsValue::from_str("toString")) {
+            if to_string_val.is_function() {
+                let func: js_sys::Function = to_string_val.unchecked_into();
+                if let Ok(res) = js_sys::Reflect::apply(&func, arg, &js_sys::Array::new()) {
+                    if let Some(s) = res.as_string() {
+                        return Ok(s);
+                    }
                 }
             }
         }
     }
     Err(js_sys::Error::new("PhyFunction.call: each argument must be a Quantity or a string").into())
+}
+
+fn is_quantity_instance(arg: &JsValue) -> bool {
+    if let Ok(constructor) = js_sys::Reflect::get(arg, &JsValue::from_str("constructor")) {
+        if let Ok(name) = js_sys::Reflect::get(&constructor, &JsValue::from_str("name")) {
+            if let Some(s) = name.as_string() {
+                return s == "Quantity";
+            }
+        }
+    }
+    false
 }
 
 
